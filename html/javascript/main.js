cd120c915619aa282e9c9086857843c3ca7da71d
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-10-10 22:52:58 -0600

    changing the layout of page

---
+++ html/javascript/main.js
@@ -0,0 +1,21 @@
+var main = (function(ev) {
+  'use  strict';
+
+  function navResponse(ev){
+    var openSection = document.getElementsByClassName('open-section')[0];
+    openSection.classList.add('closed-section');
+    openSection.classList.remove('open-section');
+    var tab = ev.target;
+    document.getElementsByClassName('select')[0].classList.remove('select');
+    tab.classList.add('select');
+    var section = tab.getAttribute("data-section");
+    document.getElementById(section).classList.add('open-section');
+    document.getElementById(section).classList.remove('closed-section');
+  }
+  return {
+    start:function(){
+      document.getElementById('nav-bar').addEventListener('click',navResponse);
+    }
+  }
+})();
+document.addEventListener('DOMContentLoaded', main.start);
